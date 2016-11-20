d22240bc2b1f2cb8dacbe07451fa16910f691dad
Ben Blamey <blamey.ben@gmail.com>
2016-11-20 09:22:30 +0100

    add make new folder sendto script

---
+++ toshiba-laptop/send-10-to-new-folder.bat
@@ -0,0 +1,11 @@
+set dir=%~dp1%random%
+mkdir "%dir%"
+move %1 "%dir%\"
+move %2 "%dir%\"
+move %3 "%dir%\"
+move %4 "%dir%\"
+move %5 "%dir%\"
+move %6 "%dir%\"
+move %7 "%dir%\"
+move %8 "%dir%\"
+move %9 "%dir%\"
